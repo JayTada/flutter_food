94a9dc76e0a44bd8c8b162ce6485124ee6ed217b
JayTada <arenajj3@gmail.com>
2022-03-07 23:35:03 +0700

    Initial commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_food/pages/home/home_page.dart';
+import 'pages/home/home_page.dart';
+
 void main() {
   runApp(const MyApp());
 }
@@ -7,7 +8,6 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -15,8 +15,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.yellow,
       ),
-      home: const HomePage(),
+      home: HomePage(),
     );
   }
 }
-
